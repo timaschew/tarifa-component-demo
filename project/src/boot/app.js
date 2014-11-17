3728601c2d290409a0e36f9f5df94c4b4a41b8d7
Anton Wilhelm <timaschew@gmail.com>
2014-11-17 17:08:08 +0100

    reduce to component/domify and query instead of component/dom fix long initial splash screen

--- project/src/boot/app.js
+++ project/src/boot/app.js
@@ -1,26 +1,24 @@
 var settings = require('tarifa-settings');
-var dom = require('component-dom');
+var domify = require('component-domify');
+var query = require('component-query');
 var afterTransition = require('after-transition');
 var template = require('./settings');
 
-var $node = dom(template);
-$node.find('dd')[0].innerText = settings.product_name;
-$node.find('dd')[1].innerText = settings.id;
-$node.find('dd')[2].innerText = settings.version;
+// this module is be loaded at the endn of the app.js (autorequire)
+module.exports = (function() {
+document.addEventListener('deviceready', function () {
+    var node = domify(template);
+    var infoEl = query('p.info');
+    infoEl.appendChild(node);
+    query.all('dd', node)[0].innerText = settings.product_name;
+    query.all('dd', node)[1].innerText = settings.id;
+    query.all('dd', node)[2].innerText = settings.version;
+    var circle = query('.circle');
 
-var $infoEl = dom('p.info');
-$infoEl.append($node);
-
-var circle = dom('.circle')[0];
-
-// TODO: there is no splashscreen, why?
-if (navigator.splashscreen && navigator.splashscreen.hide ) {
-  navigator.splashscreen.hide();
-}
-
-//TODO: debug why splash screen takes so long and how to register to an event
-// when the app is ready
-afterTransition(circle, function() {
-  $infoEl.addClass('fadeIn');
-});
-dom(circle).addClass('animate');
+    navigator.splashscreen.hide();
+    afterTransition(circle, function() {
+      infoEl.classList.add('fadeIn');
+    });
+    circle.classList.add('animate');
+  });    
+})();
